005f6d3327edddceb0a8ec61473b8ba2706cb6bd
Karina Verkholomova <karina.verkholomova@gmail.com>
2025-10-20 15:52:24 +0200

    feat:wrap MyApp with ThemeProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:coffee_app/my_app.dart';
+import 'package:coffee_app/providers/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (_) => ThemeProvider(),
+    child: const MyApp(),
+  ),);
 }
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,17 +1,19 @@
 
 import 'package:coffee_app/features/home/home_screen.dart';
+import 'package:coffee_app/providers/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      theme: themeProvider.themeData,
+      home: const MyHomePage(title: 'Coffee'),
     );
   }
 }
