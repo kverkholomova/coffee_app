44ebfaad9533fe226fcc5301780300d49e88c462
Karina Verkholomova <karina.verkholomova@gmail.com>
2025-11-20 17:08:32 +0100

    feat: Initialize the Supabase client

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:coffee_app/my_app.dart';
 import 'package:coffee_app/providers/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'YOUR_SUPABASE_URL',
+    anonKey: 'YOUR_SUPABASE_PUBLISHABLE_KEY',  );
   runApp(ChangeNotifierProvider(
     create: (_) => ThemeProvider(),
     child: const MyApp(),
