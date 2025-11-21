2d1be67230ec64e55945ad3e8917582a965547b4
Karina Verkholomova <karina.verkholomova@gmail.com>
2025-11-21 13:38:45 +0100

    chore: add env file

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:coffee_app/my_app.dart';
 import 'package:coffee_app/providers/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 
 Future<void> main() async {
+  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-    url: 'YOUR_SUPABASE_URL',
-    anonKey: 'YOUR_SUPABASE_PUBLISHABLE_KEY',  );
+    url: "${dotenv.env['URL']}",
+    anonKey: "${dotenv.env['ANON_KEY']}",  );
   runApp(ChangeNotifierProvider(
     create: (_) => ThemeProvider(),
     child: const MyApp(),
